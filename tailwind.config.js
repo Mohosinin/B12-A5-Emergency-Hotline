64276c7a95611499bed8748ad77706222ffe601e
Mohosinin <mohosinin.islam34@gmai.com>
2025-08-28 16:34:14 +0600

    done whit full respnosiv,given id and other

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'hind-madurai': ['"Hind Madurai"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+};
